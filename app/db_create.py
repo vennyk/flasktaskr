3fb8a031c73238ea72482dcb10dbd4b52265c1ef
vennyk <venny@seas.upenn.edu>
2016-08-22 10:11:19 +0530

    midway through part 2

--- app/db_create.py
+++ app/db_create.py
@@ -1,8 +1,13 @@
-import sqlite3
-from config import DATABASE_PATH
+# project/db_create.py
 
-with sqlite3.connect(DATABASE_PATH) as conn:
-	c = conn.cursor()
-	c.execute("""  CREATE TABLE tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL) """)
-	c.execute('INSERT INTO tasks (name, due_date, priority, status) VALUES ("finish tutorial", "08/25/2016", 10,1)')
-	c.execute('INSERT INTO tasks (name, due_date, priority, status) VALUES ("finish next tutorial", "09/25/2016", 10,1)')
+
+from views import db
+from models import Task
+from datetime import date
+
+db.create_all()
+
+db.session.add(Task("finish tutorial", date(2014,3,13),10,1))
+db.session.add(Task("finish real python", date(2014,3,13),10,1))
+
+db.session.commit()
--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,23 @@
-# forms.py
+# project/forms.py
+
 
 from flask_wtf import Form
-from wtforms import TextField, DateField, IntegerField, \
+from wtforms import StringField, DateField, IntegerField, \
     SelectField
 from wtforms.validators import DataRequired
 
 
 class AddTaskForm(Form):
-    task_id = IntegerField('Priority')
-    name = TextField('Task Name', validators=[DataRequired()])
+    task_id = IntegerField()
+    name = StringField('Task Name', validators=[DataRequired()])
     due_date = DateField(
         'Date Due (mm/dd/yyyy)',
         validators=[DataRequired()], format='%m/%d/%Y'
     )
     priority = SelectField(
         'Priority',
-        validators=[DataRequired()],choices=[
+        validators=[DataRequired()],
+        choices=[
             ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
             ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')
         ]
